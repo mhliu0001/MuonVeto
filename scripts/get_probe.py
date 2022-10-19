510d887ef2ccb00711675e6c5e5f11d4a249225f
mhliu0001 <mh-liu20@mails.tsinghua.edu.cn>
2022-10-19 12:59:03 +0800

    Completed get_probe.py

--- scripts/get_probe.py
+++ scripts/get_probe.py
@@ -5,9 +5,12 @@ get_probe: From probe files, use interpolation to create an LCE map for any loca
 import json
 import os
 import numpy as np
+import numpy.ma as ma 
 import scipy.interpolate
+from scipy.spatial import KDTree
 from tqdm import tqdm
 from matplotlib import pyplot as plt
+from matplotlib import colors
 
 def wc_count(path):
     '''
@@ -29,10 +32,11 @@ class get_probe:
           data_type: must be 'probe' or 'random'
           method   : must be 'grid-linear', 'RBF' or 'inverse_distance' 
         '''
+        self.method = method
 
         # Check args
         if(data_type not in ['probe', 'random']): raise ValueError(f'Unexpected data_type "{data_type}"')
-        if(method not in ['grid_linear', 'RBF', 'inverse_distance']): raise NotImplementedError(f'Method "{method}" not supported')
+        if(method not in ['grid_linear', 'RBF', 'inverse_distance', 'random_average']): raise NotImplementedError(f'Method "{method}" not supported')
         if(data_type == 'random' and method == 'grid_linear'): raise ValueError(f'data_type must be "probe" if method is "grid_linear"')
 
         print("Initializing get_probe...")
@@ -86,14 +90,13 @@ class get_probe:
         print(f'All test passed! Found {number_of_runs} runs in {data_type} mode.')
         print(f'Events per run: {events_per_run}')
         if data_type == 'probe':
-            print(f'GunXPosition/cm ranges from {self.grid_xs.min()} to {self.grid_xs.max()}')
-            print(f'GunYPosition/cm ranges from {self.grid_ys.min()} to {self.grid_ys.max()}')
-            print(f'GunZPosition/cm ranges from {self.grid_zs.min()} to {self.grid_zs.max()}')
+            print(f'GunXPosition/cm ranges from {self.grid_xs.min()} to {self.grid_xs.max()}, step is {self.grid_xs[1]-self.grid_xs[0]}')
+            print(f'GunYPosition/cm ranges from {self.grid_ys.min()} to {self.grid_ys.max()}, step is {self.grid_ys[1]-self.grid_ys[0]}')
+            print(f'GunZPosition/cm ranges from {self.grid_zs.min()} to {self.grid_zs.max()}, step is {self.grid_zs[1]-self.grid_zs[0]}')
         
         # Initialize data
         print('Reading data...')
         if data_type == 'probe':
-            self.points = np.zeros((number_of_runs,3))
             self.probes = np.zeros((number_of_runs,2))
         else:
             self.points = np.zeros((np.sum(events_per_run),3))
@@ -104,7 +107,6 @@ class get_probe:
             if data_type == 'probe':
                 with open(f"{data_path}/run{runNb}/RunConditions.json", "r") as run_conditions_file:
                     run_conditions = json.load(run_conditions_file)
-                    self.points[runNb] = run_conditions['GunXPosition/cm'], run_conditions['GunYPosition/cm'], run_conditions['GunZPosition/cm']
                 for SiPMNb in range(2):
                     with open(f"{data_path}/run{runNb}/PEs of SiPM/SiPM_{SiPMNb}.csv", "r") as PE_file:
                         probe = np.loadtxt(PE_file, dtype=int, comments='#', delimiter=',')
@@ -120,33 +122,90 @@ class get_probe:
                     with open(f"{data_path}/run{runNb}/PEs of SiPM/SiPM_{SiPMNb}.csv", "r") as PE_file:
                         probe = np.loadtxt(PE_file, dtype=int, comments='#', delimiter=',')
                         self.probes[event_index[runNb]:event_index[runNb+1], SiPMNb] = probe
-            
-        print(self.probes)
-        print(self.points)
-        
+ 
         # Initialize interpolators
-        '''
         if method == 'grid_linear':
-            self.interpolators = scipy.interpolate.LinearNDInterpolator(self.points, self.probes[:, 0]), scipy.interpolate.LinearNDInterpolator(self.points, self.probes[:, 1])
-        else if method == ''
-        '''
+            self.interpolators = (
+                scipy.interpolate.RegularGridInterpolator(
+                    (self.grid_xs, self.grid_ys, self.grid_zs), 
+                    self.probes[:,0].reshape(
+                        (self.grid_xs.shape[0], self.grid_ys.shape[0], self.grid_zs.shape[0]),
+                    )/40000,
+                    bounds_error=False,
+                    fill_value=None
+                ),
+                scipy.interpolate.RegularGridInterpolator(
+                    (self.grid_xs, self.grid_ys, self.grid_zs), 
+                    self.probes[:,1].reshape(
+                        (self.grid_xs.shape[0], self.grid_ys.shape[0], self.grid_zs.shape[0]),
+                    )/40000,
+                    bounds_error=False,
+                    fill_value=None
+                )
+            )
+        elif method == 'RBF':
+            self.interpolators = (
+                scipy.interpolate.RBFInterpolator(
+                    self.points,
+                    self.probes[:,0],
+                    neighbors=min(100, self.points.shape[0]),
+                )/40000,
+                scipy.interpolate.RBFInterpolator(
+                    self.points,
+                    self.probes[:,1],
+                    neighbors=min(100, self.points.shape[0]),
+                )/40000
+            )
+        elif method == 'inverse_distance' or method == 'random_average':
+            self.normalized_points = self.points/np.max(self.points, axis=0)
+            self.kdtree = KDTree(self.normalized_points, copy_data=True)
 
-    def __call__(self, *args, **kwds):
+    def __call__(self, points, **kwds):
         '''
         __call__: get probe at (x, y, z)
-        
-        Parameters: points: 
-        '''
-        return self.linearNDInterpolator_0(args[0]), self.linearNDInterpolator_1(args[0])
 
+        Parameters: points: (n, 3) ndarray
+        '''
+        if self.method == 'inverse_distance':
+            distances, indices = self.kdtree.query(points, k=1000, workers=30)
+            print(distances, distances.shape)
+            distances = np.clip(distances, 1e-3, None)
+            return np.sum(self.probes[indices, 0]/40000/distances, axis=-1)/np.sum(1/distances, axis=-1), np.sum(self.probes[indices, 1]/distances, axis=-1)/np.sum(1/distances, axis=-1)
+        elif self.method == 'random_average':
+            distances, indices = self.kdtree.query(points, k=1000, workers=30)
+            masked_probes_0, masked_probes_1 = ma.masked_where(distances>0.1, self.probes[indices, 0]/40000), ma.masked_where(distances>0.1, self.probes[indices, 1]/40000)
+            return ma.average(masked_probes_0, axis=-1), ma.average(masked_probes_1, axis=-1)
+        return self.interpolators[0](points), self.interpolators[1](points)
 
 if __name__ == "__main__":
     # unit test
-    a = get_probe("../random", 'random', 'inverse_distance')
-    b = get_probe("../probe", 'probe', 'inverse_distance')
-    x = np.arange(-4.9, 4.9, 0.01)
-    y = np.arange(-0.9, 0.9, 0.01)
-    z = np.arange(-49, 49, 1)
+    a = get_probe("../random", 'random', 'random_average')
+    b = get_probe("../probe", 'probe', 'grid_linear')
+    x = np.arange(-5, 5.01, 0.01)
+    y = np.arange(-0.9, 0.9, 0.1)
+    z = np.arange(-50, 50.1, 0.1)
+    xs, ys, zs = np.meshgrid(x,0,z,indexing='ij')
+    points = np.stack((xs, ys, zs), axis=-1).reshape(-1,3)
+    normalized_points = np.stack((xs/5, ys, zs/50), axis=-1).reshape(-1,3)
+    print(points)
+    
+    probe_0_rand, probe_1_rand = a(normalized_points)
+    probe_0_grid, probe_1_grid = b(points)
+
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    ax.axis('equal')
+    image = ax.scatter(points[:, 0], points[:, 2], s=0.01, c=probe_0_rand, cmap='jet', norm=colors.LogNorm())
+    fig.colorbar(image)
+    plt.savefig('../figure/random_200w_xz.png', dpi=600, bbox_inches='tight')
+    '''
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    ax.axis('equal')
+    image = ax.scatter(points[:, 0], points[:, 2], s=0.01, c=(probe_0_grid-probe_0_rand)/probe_0_rand, cmap='jet')
+    fig.colorbar(image)
+    plt.savefig('../figure/diff_rand_grid.png', dpi=600, bbox_inches='tight')
+    '''
     '''
     probe_0, probe_1 = a(np.array([0,0,0]))
     xs, ys, zs = np.meshgrid(x, y, z)
